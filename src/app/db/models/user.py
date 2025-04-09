74b3b50031272128b52d0d5a58ccbfc3ec36d2b5
sbkubric <sbkubric@gmail.com>
2025-04-09 20:14:23 +0300

    fix: timezone-aware model

--- src/app/db/models/user.py
+++ src/app/db/models/user.py
@@ -16,8 +16,10 @@ class User(BigIntBase):
     name: Mapped[str | None] = mapped_column(nullable=True, default=None)
     surname: Mapped[str | None] = mapped_column(nullable=True, default=None)
     hashed_password: Mapped[str | None] = mapped_column(String(length=255), nullable=True, default=None)
-    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
 
     @hybrid_property
     def has_password(self) -> bool:
