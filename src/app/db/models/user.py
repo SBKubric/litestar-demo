f54054f7a0e7bd402dbef036d372d1d30d25242c
sbkubric <sbkubric@gmail.com>
2025-04-09 03:59:32 +0300

    feat: user: alchemy model

---
+++ src/app/db/models/user.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from datetime import date, datetime
+
+from advanced_alchemy.base import BigIntBase
+from sqlalchemy import String
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class User(BigIntBase):
+    __tablename__ = "user_account"
+    __table_args__ = {"comment": "User accounts"}
+
+    name: Mapped[str | None] = mapped_column(nullable=True, default=None)
+    surname: Mapped[str | None] = mapped_column(nullable=True, default=None)
+    hashed_password: Mapped[str | None] = mapped_column(String(length=255), nullable=True, default=None)
+    created_at: Mapped[date] = mapped_column(default=datetime.now)
+    updated_at: Mapped[date] = mapped_column(default=datetime.now)
+
+    @hybrid_property
+    def has_password(self) -> bool:
+        return self.hashed_password is not None
