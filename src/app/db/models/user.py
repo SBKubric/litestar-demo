a5242d2e576b829cd4dd2aab7663780fc1392888
sbkubric <sbkubric@gmail.com>
2025-04-09 18:32:07 +0300

    fix: linters

--- src/app/db/models/user.py
+++ src/app/db/models/user.py
@@ -1,14 +1,11 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from datetime import datetime  # noqa: TC003
 
 from advanced_alchemy.base import BigIntBase
-from sqlalchemy import String, func
+from sqlalchemy import DateTime, String, func
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import Mapped, mapped_column
-
-if TYPE_CHECKING:
-    from datetime import datetime
 
 
 class User(BigIntBase):
@@ -19,8 +16,8 @@ class User(BigIntBase):
     name: Mapped[str | None] = mapped_column(nullable=True, default=None)
     surname: Mapped[str | None] = mapped_column(nullable=True, default=None)
     hashed_password: Mapped[str | None] = mapped_column(String(length=255), nullable=True, default=None)
-    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
 
     @hybrid_property
     def has_password(self) -> bool:
--- src/app/domain/accounts/urls.py
+++ src/app/domain/accounts/urls.py
@@ -1,5 +1,4 @@
 ACCOUNT_LOGIN = "/api/access/login"
-ACCOUNT_LOGOUT = "/api/access/logout"
 ACCOUNT_REGISTER = "/api/access/signup"
 ACCOUNT_PROFILE = "/api/me"
 ACCOUNT_LIST = "/api/users"
